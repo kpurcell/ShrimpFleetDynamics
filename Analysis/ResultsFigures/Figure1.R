97ad93b46d6ea7564de339930df24c40141b79dd
kpurcell <github@kevin-purcell.com>
2014-12-18 13:13:30 -0500

    re-ordering Fig1 as per JKC comments

--- Analysis/ResultsFigures/Figure1.R
+++ Analysis/ResultsFigures/Figure1.R
@@ -18,49 +18,46 @@ pdf(file="C:\\Users\\Kevin.Purcell\\Documents\\GitHub\\NOAA_Shrimp_ELB_Analysis\
 #            family="Arial",
 #            pointsize=12)
 
-par(mfrow=c(4,2))
-
-#DO
-plot(nspm.2mo.dur.la, select=1, shade=T, all.terms=T, scale=0,rug=F,
-     xlab=expression(paste("Dissolved Oxygen" ~ (mg/L^{2}))),
-     ylab="s(Dissolved Oxygen)")
-text(6.2, 0.12, "(a)", cex=1,font=2)
-plot(nspm.2mo.dur.tx, select=1, shade=T, all.terms=T, scale=0,rug=F,
-     xlab=expression(paste("Dissolved Oxygen" ~ (mg/L^{2}))),
-     ylab="")
-text(6.7, 0.19, "(b)", cex=1,font=2)
-
+par(mfrow=c(3,2))
 
 #Depth
 plot(nspm.2mo.dur.la, select=2, shade=T, all.terms=T, scale=0,rug=F,
      xlab="Depth (m)",
      ylab="s(Depth)")
-text(95, 0.41, "(c)", cex=1,font=2)
+text(95, 0.41, "(a)", cex=1,font=2)
 plot(nspm.2mo.dur.tx, select=2, shade=T, all.terms=T, scale=0,rug=F,
      xlab="Depth (m)",
      ylab="")
-text(82, 0.48, "(d)", cex=1,font=2)
-
-
-#Price
-plot(nspm.2mo.dur.la, select=3, shade=T, all.terms=T, scale=0,rug=F,
-     xlab="Price per pound (USD)",
-     ylab="s(Price)")
-text(5.8, 0.2, "(e)", cex=1,font=2)
-plot(nspm.2mo.dur.tx, select=3, shade=T, all.terms=T, scale=0,rug=F,
-     xlab="Price per pound (USD)",
-     ylab="")
-text(5.2, 0.22, "(f)", cex=1,font=2)
-
+text(82, 0.48, "(b)", cex=1,font=2)
 
 #Day of Year
 plot(nspm.2mo.dur.la, select=4, shade=T, all.terms=T, scale=0,rug=F,
      xlab="Julian Day",
      ylab="s(Julian Day)")
-text(241, 0.15, "(g)", cex=1,font=2)
+text(241, 0.15, "(c)", cex=1,font=2)
 plot(nspm.2mo.dur.tx, select=4, shade=T, all.terms=T, scale=0,rug=F,
      xlab="Julian Day",
      ylab="")
-text(242,0.17, "(h)", cex=1,font=2)
+text(242,0.17, "(d)", cex=1,font=2)
+
+#DO
+plot(nspm.2mo.dur.la, select=1, shade=T, all.terms=T, scale=0,rug=F,
+     xlab=expression(paste("Dissolved Oxygen" ~ (mg/L^{2}))),
+     ylab="s(Dissolved Oxygen)")
+text(6.2, 0.12, "(e)", cex=1,font=2)
+plot(nspm.2mo.dur.tx, select=1, shade=T, all.terms=T, scale=0,rug=F,
+     xlab=expression(paste("Dissolved Oxygen" ~ (mg/L^{2}))),
+     ylab="")
+text(6.7, 0.19, "(f)", cex=1,font=2)
+
+# #Price
+# plot(nspm.2mo.dur.la, select=3, shade=T, all.terms=T, scale=0,rug=F,
+#      xlab="Price per pound (USD)",
+#      ylab="s(Price)")
+# text(5.8, 0.2, "(e)", cex=1,font=2)
+# plot(nspm.2mo.dur.tx, select=3, shade=T, all.terms=T, scale=0,rug=F,
+#      xlab="Price per pound (USD)",
+#      ylab="")
+# text(5.2, 0.22, "(f)", cex=1,font=2)
 
 dev.off()

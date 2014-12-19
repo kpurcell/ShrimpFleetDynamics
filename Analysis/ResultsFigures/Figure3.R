490aa5bc4c47079c5208f649465d79b9133df5ed
kpurcell <github@kevin-purcell.com>
2014-12-19 09:27:11 -0500

    setup to run independent, pulled color frm bkgrnd

--- Analysis/ResultsFigures/Figure3.R
+++ Analysis/ResultsFigures/Figure3.R
@@ -8,24 +8,31 @@
 # TODO -> add letters, add symbol scale, standardize surface contour colors
 #         add code for pdf creation
 ###########################################################
-pdf(file="C:\\Users\\Kevin.Purcell\\Documents\\GitHub\\NOAA_Shrimp_ELB_Analysis\\Presentation\\Article\\figure3.pdf",
-    width = 6.83, height = 9.19, 
-    family = "Arial", 
-    paper = "special", 
-    onefile = FALSE)
+load("C:\\Users\\Kevin.Purcell\\Desktop\\2014-11-04.RData")
+library(mgcv)
+library(maps)
+library(mapdata)
+
+pdf(file="C:\\Users\\Kevin.Purcell\\Documents\\GitHub\\ShrimpFleetDynamics\\Presentation\\Article\\figure3.pdf",
+    width = 9, height = 9, 
+    #family = "Arial", 
+    #paper = "special", 
+    #onefile = FALSE
+    )
 
 # postscript(file="C:\\Users\\Kevin.Purcell\\Documents\\GitHub\\NOAA_Shrimp_ELB_Analysis\\Presentation\\Article\\figure\\Figure3.ps", 
 #            horizontal=F,
 #            onefile=F,
-#            width=3.27, height=9.19,
+#            width=9, height=9,
 #            family="Arial",
 #            pointsize=12)
+#windows(width=9, height=9)
 par(mfrow=c(2,2))
 
 #Plot model predictions surfaces for LA model
 vis.gam(m2008.la, view=c("cent_lon", "cent_lat"), zlim=c(-1,5), ylim=c(28,30),
         plot.type="contour", type="response", contour="black",
-        color="gray", n.grid=50, too.far=0.4,
+        color="bw", n.grid=50, too.far=0.4,
         main="High Hypoxia", ylab=expression(paste("Latitude" ~ (degree~N))), xlab=expression(paste("Longitude" ~ (degree~W))))
 map("worldHires", fill=T, col="grey",add=T)
 text(-93.5, 29.94, "(a)", cex=1,font=2)
@@ -33,7 +40,7 @@ text(-93.5, 29.94, "(a)", cex=1,font=2)
 #Plot model predicted surface for Tx model
 vis.gam(m2009.la, view=c("cent_lon", "cent_lat"),zlim=c(-1,5),ylim=c(28,30),
         plot.type="contour", type="response", contour="black",
-        color="gray", n.grid=50, too.far=0.4,
+        color="bw", n.grid=50, too.far=0.4,
         main="Low Hypoxia", ylab=expression(paste("Latitude" ~ (degree~N))), xlab=expression(paste("Longitude" ~ (degree~W))))
 #add coastline
 map("worldHires", fill=T, col="grey",add=T)
@@ -41,7 +48,7 @@ text(-93.5, 29.94, "(b)", cex=1,font=2)
 
 vis.gam(m2008.tx, view=c("cent_lon", "cent_lat"),zlim=c(-1,5),
         plot.type="contour", type="response", contour="black",
-        color="gray", n.grid=50, too.far=0.4,
+        color="bw", n.grid=50, too.far=0.4,
         main="", ylab=expression(paste("Latitude" ~ (degree~N))), xlab=expression(paste("Longitude" ~ (degree~W))))
 #add coastline
 map("worldHires", fill=T, col="grey",add=T)
@@ -49,7 +56,7 @@ text(-97.1, 29.58, "(c)", cex=1,font=2)
 #Plot model predictions surfaces for LA model
 vis.gam(m2009.tx, view=c("cent_lon", "cent_lat"), zlim=c(-1,5),
         plot.type="contour", type="response", contour="black",
-        color="gray", n.grid=50, too.far=0.4,
+        color="bw", n.grid=50, too.far=0.4,
         main="", ylab=expression(paste("Latitude" ~ (degree~N))), xlab=expression(paste("Longitude" ~ (degree~W))))
 
 #add coastline

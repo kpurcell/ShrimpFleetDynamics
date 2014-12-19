06ecb3763bf44be2cc56d176081f5deca9e6d92e
kpurcell <github@kevin-purcell.com>
2014-12-19 09:40:19 -0500

    added subtitles

--- Analysis/ResultsFigures/Figure4.R
+++ Analysis/ResultsFigures/Figure4.R
@@ -9,44 +9,47 @@
 ###########################################################
 #load(file.choose())  #load R dat file
 
-pdf(file="C:\\Users\\Kevin.Purcell\\Documents\\GitHub\\NOAA_Shrimp_ELB_Analysis\\Presentation\\Article\\figure4.pdf",
-    width = 4.86, height = 9.19, 
-    family = "Arial", 
-    paper = "special", 
-    onefile = FALSE)
+pdf(file="C:\\Users\\Kevin.Purcell\\Documents\\GitHub\\ShrimpFleetDynamics\\Presentation\\Article\\figure4.pdf",
+    width = 8, height = 8, 
+   # family = "Arial", 
+    #paper = "special", 
+    #onefile = FALSE
+   )
 
-postscript(file="C:\\Users\\Kevin.Purcell\\Documents\\GitHub\\NOAA_Shrimp_ELB_Analysis\\Presentation\\Article\\figure\\Figure4.ps", 
-           horizontal=F,
-           onefile=F,
-           width=3.27, height=9.19,
-           family="Arial",
-           pointsize=12)
+# postscript(file="C:\\Users\\Kevin.Purcell\\Documents\\GitHub\\NOAA_Shrimp_ELB_Analysis\\Presentation\\Article\\figure\\Figure4.ps", 
+#            horizontal=F,
+#            onefile=F,
+#            width=3.27, height=9.19,
+#            family="Arial",
+#            pointsize=12)
 
 par(mfrow=c(2,2))
 
 #DO
 plot(nspm.2mo.avg.dur.la, select=1, shade=T, all.terms=T, scale=0, rug=F,
-     xlab=expression(paste("Dissolved Oxygen" ~ (mg/L^{2}))),
+     xlab=expression(paste("Dissolved Oxygen" ~ (mgL^{-1}))),
      ylab="s(Average Tow Duration)",
-     ylim=c(-0.35,0.23))
+     ylim=c(-0.35,0.23),
+     main="Louisiana")
 #abline(h=0)
 text(0.49, .22, "(a)", cex=1,font=2)
 plot(nspm.2mo.avg.dur.tx, select=1, shade=T, all.terms=T, scale=0,rug=F,
-     xlab=expression(paste("Dissolved Oxygen"~ (mg/L^{2}))),
+     xlab=expression(paste("Dissolved Oxygen"~ (mgL^{-1}))),
      ylab="s(Average Tow Duration)",
-     ylim=c(-0.35,0.23))
+     ylim=c(-0.35,0.23),
+     main="Texas")
 #abline(h=0)
 text(1.8, .22, "(b)", cex=1,font=2)
 
 #DO
 plot(nspm.2mo.tow.count.la, select=1, shade=T, all.terms=T, scale=0,rug=F,
-     xlab=expression(paste("Dissolved Oxygen"~ (mg/L^{2}))),
+     xlab=expression(paste("Dissolved Oxygen"~ (mgL^{-1}))),
      ylab="s(Tow Count)",
      ylim=c(-1.2,0.22))
 #abline(h=0)
 text(0.55,0.22, "(c)", cex=1,font=2)
 plot(nspm.2mo.tow.count.tx, select=1, shade=T, all.terms=T, scale=0,rug=F,
-     xlab=expression(paste("Dissolved Oxygen"~ (mg/L^{2}))),
+     xlab=expression(paste("Dissolved Oxygen"~ (mgL^{-1}))),
      ylab="s(Tow Count)")
 #abline(h=0)
 text(1.75, .35, "(d)", cex=1,font=2)

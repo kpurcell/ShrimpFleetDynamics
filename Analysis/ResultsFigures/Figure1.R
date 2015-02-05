59048498eac6e349c8d91ab5f1efa02c26e9f8a6
kpurcell <github@kevin-purcell.com>
2015-02-05 10:35:42 -0500

    revised figure 1 re: JKC comments

--- Analysis/ResultsFigures/Figure1.R
+++ Analysis/ResultsFigures/Figure1.R
@@ -24,33 +24,35 @@ pdf(file="C:\\Users\\Kevin.Purcell\\Documents\\GitHub\\ShrimpFleetDynamics\\Pres
 par(mfrow=c(3,2))
 
 #Depth
-plot(nspm.2mo.dur.la, select=2, shade=T, all.terms=T, scale=0,rug=F,
+plot(nspm.2mo.dur.la, select=2, shade=T, all.terms=T, scale=0,rug=T,
      xlab="Depth (m)",
-     ylab="s(Depth)")
+     ylab="s(Depth)",
+     main="Louisiana")
 text(95, 0.41, "(a)", cex=1,font=2)
 
-plot(nspm.2mo.dur.tx, select=2, shade=T, all.terms=T, scale=0,rug=F,
+plot(nspm.2mo.dur.tx, select=2, shade=T, all.terms=T, scale=0,rug=T,
      xlab="Depth (m)",
-     ylab="")
+     ylab="",
+     main="Texas")
 text(82, 0.48, "(b)", cex=1,font=2)
 
 #Day of Year
-plot(nspm.2mo.dur.la, select=4, shade=T, all.terms=T, scale=0,rug=F,
+plot(nspm.2mo.dur.la, select=4, shade=T, all.terms=T, scale=0,rug=T,
      xlab="Julian Day",
      ylab="s(Julian Day)")
 text(241, 0.15, "(c)", cex=1,font=2)
-plot(nspm.2mo.dur.tx, select=4, shade=T, all.terms=T, scale=0,rug=F,
+plot(nspm.2mo.dur.tx, select=4, shade=T, all.terms=T, scale=0,rug=T,
      xlab="Julian Day",
      ylab="")
 text(242,0.17, "(d)", cex=1,font=2)
 
 #DO
-plot(nspm.2mo.dur.la, select=1, shade=T, all.terms=T, scale=0,rug=F,
-     xlab=expression(paste("Dissolved Oxygen" ~ (mg/L^{2}))),
+plot(nspm.2mo.dur.la, select=1, shade=T, all.terms=T, scale=0,rug=T,
+     xlab=expression(paste("Dissolved Oxygen" ~ (mgL^{-1}))),
      ylab="s(Dissolved Oxygen)")
 text(6.2, 0.12, "(e)", cex=1,font=2)
-plot(nspm.2mo.dur.tx, select=1, shade=T, all.terms=T, scale=0,rug=F,
-     xlab=expression(paste("Dissolved Oxygen" ~ (mg/L^{2}))),
+plot(nspm.2mo.dur.tx, select=1, shade=T, all.terms=T, scale=0,rug=T,
+     xlab=expression(paste("Dissolved Oxygen" ~ (mgL^{-1}))),
      ylab="")
 text(6.7, 0.19, "(f)", cex=1,font=2)
 

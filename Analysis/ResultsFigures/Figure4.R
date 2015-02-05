2ebf0a581b2066a02bf40865ae704ce108609a9c
kpurcell <github@kevin-purcell.com>
2015-02-05 10:56:42 -0500

    added rug plots to all panels

--- Analysis/ResultsFigures/Figure4.R
+++ Analysis/ResultsFigures/Figure4.R
@@ -26,14 +26,14 @@ pdf(file="C:\\Users\\Kevin.Purcell\\Documents\\GitHub\\ShrimpFleetDynamics\\Pres
 par(mfrow=c(2,2))
 
 #DO
-plot(nspm.2mo.avg.dur.la, select=1, shade=T, all.terms=T, scale=0, rug=F,
+plot(nspm.2mo.avg.dur.la, select=1, shade=T, all.terms=T, scale=0, rug=T,
      xlab=expression(paste("Dissolved Oxygen" ~ (mgL^{-1}))),
      ylab="s(Average Tow Duration)",
      ylim=c(-0.35,0.23),
      main="Louisiana")
 #abline(h=0)
 text(0.49, .22, "(a)", cex=1,font=2)
-plot(nspm.2mo.avg.dur.tx, select=1, shade=T, all.terms=T, scale=0,rug=F,
+plot(nspm.2mo.avg.dur.tx, select=1, shade=T, all.terms=T, scale=0,rug=T,
      xlab=expression(paste("Dissolved Oxygen"~ (mgL^{-1}))),
      ylab="s(Average Tow Duration)",
      ylim=c(-0.35,0.23),
@@ -42,13 +42,13 @@ plot(nspm.2mo.avg.dur.tx, select=1, shade=T, all.terms=T, scale=0,rug=F,
 text(1.8, .22, "(b)", cex=1,font=2)
 
 #DO
-plot(nspm.2mo.tow.count.la, select=1, shade=T, all.terms=T, scale=0,rug=F,
+plot(nspm.2mo.tow.count.la, select=1, shade=T, all.terms=T, scale=0,rug=T,
      xlab=expression(paste("Dissolved Oxygen"~ (mgL^{-1}))),
      ylab="s(Tow Density)",
      ylim=c(-1.2,0.22))
 #abline(h=0)
 text(0.55,0.22, "(c)", cex=1,font=2)
-plot(nspm.2mo.tow.count.tx, select=1, shade=T, all.terms=T, scale=0,rug=F,
+plot(nspm.2mo.tow.count.tx, select=1, shade=T, all.terms=T, scale=0,rug=T,
      xlab=expression(paste("Dissolved Oxygen"~ (mgL^{-1}))),
      ylab="s(Tow Density)")
 #abline(h=0)
